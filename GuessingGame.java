faa0ed3f299075054c2b052dcac62ea63e6f3d2c
moosecanswim <kyle.gallagher.09@gmail.com>
2017-07-25 15:14:10 -0400

    added an over range hint

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,5 +1,6 @@
 package guessingGame;
 
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -8,6 +9,7 @@ public class GuessingGame {
 	public static void main(String[] args) {
 		int guessCounter = 0;
 		int upper = 10;
+		String additionalComment= null;
 
 		Random r = new Random();
 		int x = 1+ r.nextInt(upper); //find a rando number between 1 and 10
@@ -21,12 +23,22 @@ public class GuessingGame {
 			System.out.println("Guess a number (int): ");
 			int guess = scan.nextInt();
 			guessCounter++; //adds one every time you guess
+			
+			
+			if (guess > upper ){
+				additionalComment = String.format("Yo you guessed way to high.  the range goes up to %d",  upper);
+			}
+			
 			if (guess == x){
 				System.out.println("YOU GOT IT! The number was " + x +" | It only took you " + guessCounter + " guesses!");  //include a bit about how many tries it took
 				found = true;
 			}
 			else if (guess > x){
-				
+				if (guess > upper ){
+					additionalComment = String.format("The range goes up to %d",  upper);
+					System.out.println(additionalComment);
+				}
+
 				Boolean tempLeaveDo = false;
 				do{
 					System.out.println("You guessed too high, try again? (Y/N)");
@@ -34,13 +46,13 @@ public class GuessingGame {
 					if (keepSearching.equalsIgnoreCase("n")){
 						continueSearching = false;
 						tempLeaveDo = true;
-						
+
 					}
 					else if(keepSearching.equalsIgnoreCase("y")){
 						tempLeaveDo = true;
 					}
 					else{
-						System.out.println("Sooo, do you want to keep searching? (Y/N)");
+						System.out.println("Sooo, do you want to keep searching? ");
 					}
 
 				}while (tempLeaveDo == false);
@@ -56,13 +68,13 @@ public class GuessingGame {
 					if (keepSearching.equalsIgnoreCase("n")){
 						continueSearching = false;
 						tempLeaveDo = true;
-						
+
 					}
 					else if(keepSearching.equalsIgnoreCase("y")){
 						tempLeaveDo = true;
 					}
 					else{
-						System.out.println("Sooo, do you want to keep searching? (Y/N)");
+						System.out.println("Sooo, do you want to keep searching?");
 					}
 
 				}while (tempLeaveDo == false);
@@ -72,7 +84,9 @@ public class GuessingGame {
 		}while (found == false && continueSearching == true);
 	}
 
+
+
+
+
+
 }
-
-
-//maybe add a hint ability after a certain amount of wrong answers
